97fbb3f09771c5ab7177b7a9dca4ada69d3ba75f
elvisha9 <51443986+elvisha9@users.noreply.github.com>
2019-06-07 16:12:20 +0200

    Add files via upload

--- siamese.py
+++ siamese.py
@@ -11,12 +11,12 @@ class SiameseModel(nn.Module):
         feats1 = feats1_list[self.layer_to_opt];
         feats2_list = self.model(inp2);
         feats2 = feats2_list[self.layer_to_opt];
-        print(torch.mean(feats1),torch.mean(feats2))
-        print(feats1.shape,feats2.shape)
+        # print(torch.mean(feats1),torch.mean(feats2))
+        # print(feats1.shape,feats2.shape)
         feats1 = feats1 - torch.mean(feats1,dim=0,keepdim=True)
         feats2 = feats2 - torch.mean(feats2,dim=0,keepdim=True)
-        print(torch.mean(feats1),torch.mean(feats2))
-        print(feats1.shape,feats2.shape)
+        # print(torch.mean(feats1),torch.mean(feats2))
+        # print(feats1.shape,feats2.shape)
         sim = torch.sum(feats1*feats2, dim=1)*torch.rsqrt(torch.sum(feats1*feats1,dim=1))*torch.rsqrt(torch.sum(feats2*feats2,dim=1))
         return 1-sim
         
